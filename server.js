9363e7fb01137f79a4a7de106ba225953f64f4e8
Amit Kumar Raikwar <amitkumarraikwar92@gmail.com>
2025-02-01 18:37:07 +0000

    add initial configuration files and module exports for dotenv and math intrinsics

---
+++ server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const http = require('http');
+const cors = require('cors');
+const app = require('./app');
+const port = process.env.PORT || 3000;
+
+
+// app.use(cors());
+
+const server = http.createServer(app);
+
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
