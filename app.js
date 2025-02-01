210f7dbfe7abdff0e637f484112313598d36feff
Amit Kumar Raikwar <amitkumarraikwar92@gmail.com>
2025-02-01 19:10:12 +0000

    add database connection and environment configuration; update dependencies and add new files

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const connectToDb = require('./database/db');
+
+connectToDb();
+
+
+app.use(cors());
 
 
 app.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-const dotenv = require('dotenv');
-dotenv.config();
 const http = require('http');
-const cors = require('cors');
-const app = require('./app');
 const port = process.env.PORT || 3000;
 
-
-// app.use(cors());
 
 const server = http.createServer(app);
 
